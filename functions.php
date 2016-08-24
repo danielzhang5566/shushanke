32953cdbde1d5ea7e49d2bb5362a6b2991d806d8
zeakhold <zeakhold@yeah.net>
2016-08-24 21:35:42 +0800

    关闭提示&禁止更新

--- functions.php
+++ functions.php
@@ -450,4 +450,13 @@ if($option['if_status']==1){
 	require_once('includes/post_type_status.php');
 }
 
+/*** 二次开发 ***/
+//关闭提示&禁止更新
+add_filter('pre_site_transient_update_core', create_function('$a', "return null;")); // 关闭核心提示
+add_filter('pre_site_transient_update_plugins', create_function('$a', "return null;")); // 关闭插件提示
+add_filter('pre_site_transient_update_themes', create_function('$a', "return null;")); // 关闭主题提示
+remove_action('admin_init', '_maybe_update_core'); // 禁止 WordPress 检查更新
+remove_action('admin_init', '_maybe_update_plugins'); // 禁止 WordPress 更新插件
+remove_action('admin_init', '_maybe_update_themes'); // 禁止 WordPress 更新主题
+
 ?>

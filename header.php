1ae459f69cc0761ea4021bbb07babbca487d934f
zeakhold <zeakhold@yeah.net>
2017-06-24 12:43:33 +0800

    换上压缩的style.css

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="http://cdn.bootcss.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/css/tooltipster.css" />
-		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/style.min.css" />
 		<?php wp_head();?>
 	</head>
 	<body>

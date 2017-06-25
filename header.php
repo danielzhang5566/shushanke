ea8c2b24d4f6411ab11d6f668d7777742509914b
zeakhold <zeakhold@yeah.net>
2017-06-25 13:01:09 +0800

    bug fix:移动端点击评论框输入的时候会自动放大

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<?php include('includes/seo.php'); ?>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
 		<link href="http://cdn.bootcss.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/css/tooltipster.css" />
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/style.min.css" />

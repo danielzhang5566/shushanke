cebf35e245e4e82b9661744146ae4393b032cbc4
zeakhold <zeakhold@gmail.com>
2018-11-05 02:02:24 +0800

    启用新域名susamko.com

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 #  蜀山客主题
 
 * 项目地址：https://github.com/zeakhold/shushanke
-* 博客地址：http://www.zeakhold.com
+* 博客地址：http://www.susamko.com
 * 联系邮箱：hi@zeakhold.com
 
 ### 说明
--- footer.php
+++ footer.php
@@ -1,21 +1,21 @@
         <?php
         /* 如果是【关于】页面，则在底部添加乌龟部件 */
         if (is_page(146) && !wp_is_mobile()):?>
-            <object type="application/x-shockwave-flash" style="margin: 5px auto;outline:none;display: block;" data="http://www.zeakhold.com/data/turtle.swf?up_turtle5HeadColor=828250&amp;up_turtle5ShellColor=828250&amp;up_waterColor=FFFFFF&amp;up_turtle3LegColor=66663f&amp;up_turtle4ShellColor=828250&amp;up_turtle2HeadColor=828250&amp;up_percentWater=1&amp;up_turtle3HeadColor=828250&amp;up_turtle1ShellColor=828250&amp;up_foodColor=D1BB19&amp;up_turtle4HeadColor=828250&amp;up_turtle3ShellColor=828250&amp;up_turtle1HeadColor=828250&amp;up_turtle4LegColor=66663f&amp;up_turtle1LegColor=66663f&amp;up_numTurtles=2&amp;up_turtleName=Turtle&amp;up_groundColor=EEEEEE&amp;up_turtle2ShellColor=828250&amp;up_turtle2LegColor=66663f&amp;up_turtle5LegColor=66663f&amp;" width="700" height="300"><param name="movie" value="http://www.zeakhold.com/data/turtle.swf?up_turtle5HeadColor=828250&amp;up_turtle5ShellColor=828250&amp;up_waterColor=FFFFFF&amp;up_turtle3LegColor=66663f&amp;up_turtle4ShellColor=828250&amp;up_turtle2HeadColor=828250&amp;up_percentWater=1&amp;up_turtle3HeadColor=828250&amp;up_turtle1ShellColor=828250&amp;up_foodColor=D1BB19&amp;up_turtle4HeadColor=828250&amp;up_turtle3ShellColor=828250&amp;up_turtle1HeadColor=828250&amp;up_turtle4LegColor=66663f&amp;up_turtle1LegColor=66663f&amp;up_numTurtles=2&amp;up_turtleName=Turtle&amp;up_groundColor=EEEEEE&amp;up_turtle2ShellColor=828250&amp;up_turtle2LegColor=66663f&amp;up_turtle5LegColor=66663f&amp;"><param name="AllowScriptAccess" value="always"><param name="wmode" value="opaque"><param name="scale" value="noscale"><param name="salign" value="tl"></object>
+            <object type="application/x-shockwave-flash" style="margin: 5px auto;outline:none;display: block;" data="http://www.susamko.com/data/turtle.swf?up_turtle5HeadColor=828250&amp;up_turtle5ShellColor=828250&amp;up_waterColor=FFFFFF&amp;up_turtle3LegColor=66663f&amp;up_turtle4ShellColor=828250&amp;up_turtle2HeadColor=828250&amp;up_percentWater=1&amp;up_turtle3HeadColor=828250&amp;up_turtle1ShellColor=828250&amp;up_foodColor=D1BB19&amp;up_turtle4HeadColor=828250&amp;up_turtle3ShellColor=828250&amp;up_turtle1HeadColor=828250&amp;up_turtle4LegColor=66663f&amp;up_turtle1LegColor=66663f&amp;up_numTurtles=2&amp;up_turtleName=Turtle&amp;up_groundColor=EEEEEE&amp;up_turtle2ShellColor=828250&amp;up_turtle2LegColor=66663f&amp;up_turtle5LegColor=66663f&amp;" width="700" height="300"><param name="movie" value="http://www.susamko.com/data/turtle.swf?up_turtle5HeadColor=828250&amp;up_turtle5ShellColor=828250&amp;up_waterColor=FFFFFF&amp;up_turtle3LegColor=66663f&amp;up_turtle4ShellColor=828250&amp;up_turtle2HeadColor=828250&amp;up_percentWater=1&amp;up_turtle3HeadColor=828250&amp;up_turtle1ShellColor=828250&amp;up_foodColor=D1BB19&amp;up_turtle4HeadColor=828250&amp;up_turtle3ShellColor=828250&amp;up_turtle1HeadColor=828250&amp;up_turtle4LegColor=66663f&amp;up_turtle1LegColor=66663f&amp;up_numTurtles=2&amp;up_turtleName=Turtle&amp;up_groundColor=EEEEEE&amp;up_turtle2ShellColor=828250&amp;up_turtle2LegColor=66663f&amp;up_turtle5LegColor=66663f&amp;"><param name="AllowScriptAccess" value="always"><param name="wmode" value="opaque"><param name="scale" value="noscale"><param name="salign" value="tl"></object>
         <?php endif;?>
 
 		<footer class="site-footer">
 			<div class="site-info" style="line-height: 10px;">
 				Copyright © 2015-2017 <a href="<?php echo home_url();?>"  id="copyright"><?php echo get_bloginfo('name');?></a>
 				 | Thank <a href="http://zhangshiyu.name" target="_blank">Erl : )</a>
-				<p style="font-size: 13px"><a href="http://www.zeakhold.com">粤ICP备16026578号</a></p>
+				<p style="font-size: 13px"><a href="http://www.susamko.com">粤ICP备16026578号</a></p>
 		</div> <!-- .site-info -->
         </footer>
         <script src="https://cdn.bootcss.com/jquery/2.1.0/jquery.min.js"></script>
 		<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/comments-ajax.js"></script>
 		<script src="https://cdn.bootcss.com/tooltipster/3.2.6/js/jquery.tooltipster.min.js"></script>
 		<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/common.js"></script>
-        <script type="text/javascript">var _hmt=_hmt||[];(function(){var hm=document.createElement("script");hm.src="https://hm.baidu.com/hm.js?dadddb77ce27fea5a5096368b7397ce3";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s)})();</script>
+        <script type="text/javascript">var _hmt=_hmt||[];(function(){var hm=document.createElement("script");hm.src="https://hm.baidu.com/hm.js?c176aaccbb83e107b698ad0475b263a3";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s)})();</script>
 
 		<?php
 		/* 如果是网站首页、分类页面、独立页面时（蜀山客的这三类页面都设置为【有背景图】）*/
